0fb29732c0308b20ecca46c3e634ff96487b54c1
Gildeir <gildeirlopes@gmail.com>
2021-06-28 17:37:48 -0300

    corrigindo requisito 2 e controlando componentes

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,7 +3,17 @@ import PlanetContext from '../context/PlanetContext';
 import HeaderTable from './HeaderTable';
 
 function Table() {
-  const { data, setData, setFilters, filters } = useContext(PlanetContext);
+  const {
+    data,
+    setData,
+    setFilters,
+    filters,
+    column,
+    setColumn,
+    comparison,
+    setComparison,
+    value,
+    setValue } = useContext(PlanetContext);
 
   useEffect(() => {
     const getPlanet = async () => {
@@ -11,52 +21,134 @@ function Table() {
       const response = await request.json();
       const result = response.results;
       result.forEach((element) => delete element.residents);
-      console.log(result);
       setData(result);
     };
     getPlanet();
   }, [setData]);
 
-  function handleSearch({ target: { value } }) {
+  function handleSearch({ target }) {
     setFilters({
       ...filters,
-      filterByName: value.toLowerCase(),
+      filterByName: { name: target.value.toLowerCase() },
     });
   }
 
   function inputFunc() {
-    return (<input
-      data-testid="name-filter"
-      type="text"
-      onChange={ handleSearch }
-      placeholder="search..."
-    />);
+    return (
+      <label htmlFor="filter">
+        <input
+          data-testid="name-filter"
+          type="search"
+          onChange={ (handleSearch) }
+          placeholder="search..."
+          id="filter"
+        />
+      </label>
+    );
+  }
+
+  function handleClick() {
+    setFilters({
+      ...filters,
+      filterByNumericValues: [
+        ...filters.filterByNumericValues,
+        {
+          column,
+          comparison,
+          value,
+        },
+      ],
+    });
+  }
+
+  function optionsColumnFilter() {
+    const options = ['population', 'orbital_period', 'diameter',
+      'rotation_period', 'surface_water'];
+
+    return (options.map((item, index) => (
+      <option
+        key={ index }
+        id={ item }
+        name="column"
+      >
+        {item}
+      </option>
+    )));
+  }
+
+  function optionComparisonFilter() {
+    const options = ['maior que', 'menor que', 'igual a'];
+    return (options.map((method) => (
+      <option
+        key={ method }
+        id="comparison"
+        name="comparison"
+        value={ method }
+      >
+        {method}
+      </option>
+    )));
+  }
+
+  function numericFilters() {
+    return (
+      <section className="numericFilters">
+        <select
+          data-testid="column-filter"
+          onChange={ (e) => setColumn(e.target.value) }
+        >
+          {optionsColumnFilter()}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ (e) => setComparison(e.target.value) }
+        >
+          {optionComparisonFilter()}
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          name="value"
+          onChange={ (e) => setValue(e.target.value) }
+        />
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ handleClick }
+        >
+          Filtrar
+
+        </button>
+      </section>
+
+    );
+  }
+
+  function renderTable() {
+    if (data.length === 0) {
+      return data;
+    } return (
+      <tbody>
+        {data.filter((namesOfPlanets) => namesOfPlanets
+          .name.toLowerCase().includes(filters.filterByName.name))
+          .map((items, index) => (
+            <tr key={ index }>
+              {Object.values(items)
+                .map((planets) => (
+                  <td key={ value }>
+                    { planets }
+                  </td>))}
+            </tr>
+          ))}
+      </tbody>);
   }
 
   return (
     <div>
       <HeaderTable />
-      <tbody>
-        {inputFunc()}
-        {data.filter((names) => names.name.toLowerCase().includes(filters.filterByName))
-          .map((items, index) => (
-            <tr key={ index }>
-              <td>{Object.values(items.name)}</td>
-              <td>{Object.values(items.rotation_period)}</td>
-              <td>{Object.values(items.orbital_period)}</td>
-              <td>{Object.values(items.diameter)}</td>
-              <td>{Object.values(items.climate)}</td>
-              <td>{Object.values(items.gravity)}</td>
-              <td>{Object.values(items.terrain)}</td>
-              <td>{Object.values(items.surface_water)}</td>
-              <td>{Object.values(items.population)}</td>
-              <td>{Object.values(items.films)}</td>
-              <td>{Object.values(items.created)}</td>
-              <td>{Object.values(items.edited)}</td>
-              <td>{Object.values(items.url)}</td>
-            </tr>
-          ))}
-      </tbody>
+      {inputFunc()}
+      {numericFilters()}
+      {renderTable()}
     </div>
   );
 }
--- src/providers/PlanetProvider.jsx
+++ src/providers/PlanetProvider.jsx
@@ -2,11 +2,30 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PlanetContext from '../context/PlanetContext';
 
+const initialState = {
+  filterByName: { name: '' },
+  filterByNumericValues: [],
+};
+
 function PlanetProvider({ children }) {
   const [data, setData] = useState([]);
-  const [filters, setFilters] = useState([]);
+  const [filters, setFilters] = useState(initialState);
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('');
+  const [value, setValue] = useState('');
 
-  const context = { data, setData, filters, setFilters };
+  const context = {
+    data,
+    setData,
+    filters,
+    setFilters,
+    column,
+    setColumn,
+    comparison,
+    setComparison,
+    value,
+    setValue };
+
   return (
     <PlanetContext.Provider value={ context }>
       {children}
